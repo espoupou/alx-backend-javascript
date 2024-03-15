74423227bf6bca4170c5785f1d9c0c0ad57cb53f
espoupou <espoirativon@gmail.com>
2024-03-15 17:04:31 +0000

    done

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+export default class ClassRoom {
+  /**
+   * Implements a class named ClassRoom
+   * @param {Number} maxStudentsSize - max student
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
